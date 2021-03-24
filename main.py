dde62d85ffbeda09943fd3b4da6db6dd3ea99e4d
pyfuhr <susanooeditor@gmail.com>
2021-03-24 15:59:06 +0300

    It's worked :)))

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
 import rsa
-import socket
-from threading import Thread
+import socket, threading, signal
+from threading import Thread, Event
 import sys, os
-from PyQt5 import uic
+from PyQt5 import uic, QtGui, QtCore
 from PyQt5.QtCore import QUrl, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage, QWebEngineSettings
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QFileDialog
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage, QWebEngineSettings, QWebEngineDownloadItem
+
+threADs = []
+running = os.getpid()
+
+class MyThread(Thread):
+
+    # Thread class with a _stop() method.
+    # The thread itself has to check
+    # regularly for the stopped() condition.
+
+    def __init__(self, *args, **kwargs):
+        super(MyThread, self).__init__(*args, **kwargs)
+        self._stop = Event()
+
+    # function using _stop function
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+
+def closeAPP():
+    global running
+    for i in threADs:
+        i.stop()
+    os.kill(running, 1)
 
 
 def UI():
-    app = QApplication(sys.argv)
     global ex
+    app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
-    sys.exit(app.exec_())
+    app.exec_()
+    closeAPP()
+
 
 
 def potok(sc):
-    ex.list.addItem(str(sc))
+    ex.list.addItem(str(sc.getpeername()[0]))
+    ex.list.item(ex.list.count() - 1).setForeground(Qt.red)
     sc.send(str(sc).encode())
+    while True:
+        message = sc.recv(1024).decode()
+        if message:
+            if message.endswith('EXIT'):
+                break
+            settexth(ex.chatRoom, message, '#0000FF')
+            ex.chatRoom.update()
+    settexth(ex.chatRoom, 'I2CMP:Sock.close at ' + str(sc.getpeername()), '#FF0000')
+    ex.chatRoom.update()
     sc.close()
 
 
@@ -66,6 +105,28 @@ class MyWidget(QMainWindow):
 
         self.loadPage('main.html')
         self.abort.clicked.connect(self.webEngineView.stop)
+        self.reload.clicked.connect(self.webEngineView.reload)
+        self.redo.clicked.connect(self.webEngineView.back)
+
+        self.webEngineView.page().profile().downloadRequested.connect(self.on_downloadRequested)
+        self.tools.currentIndexChanged.connect(self.toolStart)
+
+    def on_downloadRequested(self, download):
+        old_path = download.path()
+        suffix = QtCore.QFileInfo(old_path).suffix()
+        path, _ = QFileDialog.getSaveFileName(self.webEngineView.page().view(), "Save File", old_path, "*."+suffix)
+        if path:
+            download.setPath(path)
+            download.accept()
+
+    def update(self) -> None:
+        self.repaint()
+
+    def toolStart(self):
+        if self.tools.currentIndex() == 1:
+            path, _ = QFileDialog.getSaveFileName(self, "Save File", os.curdir, "*.pdf")
+            self.webEngineView.page().printToPdf(path)
+        self.tools.setCurrentIndex(0)
 
     def viewmessage(self, title, text, yes):
         self.MB.setWindowTitle(title)
@@ -96,16 +157,19 @@ class MyWidget(QMainWindow):
 if __name__ == '__main__':
 
     ex = 0
-
     Thread(target=UI).start()
 
     print('Next')
 
+    iptable = {}
+
     sock = socket.socket()
-    sock.bind(('localhost', 5000))
+    sock.bind(('10.20.2.90', 5000))
     sock.listen(10)
 
     while True:
-        so, addr= sock.accept()
-        Thread(target=lambda: potok(so)).start()
-
+        so, addr = sock.accept()
+        iptable[addr] = so
+        print(iptable)
+        threADs.append(MyThread(target=lambda: potok(so)))
+        threADs[-1].start()
--- send.py
+++ send.py
@@ -2,5 +2,13 @@ import socket
 
 sock = socket.socket()
 
-sock.connect(('localhost', 5000))
+sock.connect(('10.20.2.90', 5000))
 print(sock.recv(1024).decode())
+
+while True:
+    mes = str(socket.gethostname()) + ': ' + input()
+    sock.send(mes.encode())
+    if mes.endswith('EXIT'):
+        break
+
+sock.close()
